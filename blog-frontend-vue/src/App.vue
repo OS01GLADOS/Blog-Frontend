4b317b3b204bd38684f576552e0c2c5a4c4bf5e3
OS01GLADOS <no.name.tyan.d@gmail.com>
2022-02-14 08:42:01 +0300

    deleted useless code

--- blog-frontend-vue/src/App.vue
+++ blog-frontend-vue/src/App.vue
@@ -8,33 +8,8 @@
 </template>
 
 <script>
-import showBlogsVue from "./components/Blogs/showBlogs.vue"
-import loginVue from "./components/Authentification/login.vue"
-import registrationVue from "./components/Authentification/registration.vue"
-
-const routes = {
-  '/': showBlogsVue,
-  '/login': loginVue,
-  '/register': registrationVue
-}
-
 export default {
   name: 'App',
-  data(){
-    return{
-      currentPath: window.location.hash
-    }
-  },
-  computed:{
-    currentView(){
-      return routes[this.currentPath.slice(1) || '/'] 
-    }
-  },
-  mounted() {
-    window.addEventListener('hashchange', () =>{
-      this.currentPath = window.location.hash
-    })
-  },
 }
 </script>
 
